635410fd80277e9b462053cf5421933daaf53840
Thomas <thomasculotta94@gmail.com>
2018-04-06 11:00:33 -0500

    basically created all of the base puzzle mechanics

--- Assets/Scripts/PlayerPuzzleMechanics.cs
+++ Assets/Scripts/PlayerPuzzleMechanics.cs
@@ -5,16 +5,28 @@ using UnityEngine;
 public class PlayerPuzzleMechanics : MonoBehaviour
 {
     /// <summary>
-    /// The watering can.
+    /// The watering can to pick up
     /// </summary>
     [SerializeField]
     private GameObject _wateringCan;
 
     /// <summary>
-    /// The collision object for the fountain.
+    /// The held watering can
+    /// </summary>
+    [SerializeField]
+    private GameObject _heldWateringCan;
+
+    /// <summary>
+    /// The collision object for the fountain
     /// </summary>
     [SerializeField]
     private GameObject _fountainWater;
+
+    /// <summary>
+    /// The held full watering can
+    /// </summary>
+    [SerializeField]
+    private GameObject _heldWateringCanFull;
 
     /// <summary>
     /// The collision object for the dead section of the maze
@@ -22,19 +34,209 @@ public class PlayerPuzzleMechanics : MonoBehaviour
     [SerializeField]
     private GameObject _deadGarden;
 
-    private Vector3 _rayOrigin;
+    /// <summary>
+    /// The wood log to pick up
+    /// </summary>
+    [SerializeField]
+    private GameObject _woodLog;
 
+    /// <summary>
+    /// The held wood log
+    /// </summary>
+    [SerializeField]
+    private GameObject _heldWoodLog;
+
+    /// <summary>
+    /// The table saw
+    /// </summary>
+    [SerializeField]
+    private GameObject _tableSaw;
+
+    /// <summary>
+    /// The held axe handle
+    /// </summary>
+    [SerializeField]
+    private GameObject _heldAxeHandle;
+
+    /// <summary>
+    /// The axe head to use with the handle
+    /// </summary>
+    [SerializeField]
+    private GameObject _axeHead;
+
+    /// <summary>
+    /// The held assembled axe
+    /// </summary>
+    [SerializeField]
+    private GameObject _heldAxeFinal;
+
+    /// <summary>
+    /// The tree to cut down
+    /// </summary>
+    [SerializeField]
+    private GameObject _bridgeTree;
+
+    /// <summary>
+    /// The frozen gate lock
+    /// </summary>
+    [SerializeField]
+    private GameObject _gateLock;
+
+    /// <summary>
+    /// The spring essence to find
+    /// </summary>
+    [SerializeField]
+    private GameObject _springEssence;
+
+    /// <summary>
+    /// The summer essence to find
+    /// </summary>
+    [SerializeField]
+    private GameObject _summerEssence;
+
+    /// <summary>
+    /// The fall essence to find
+    /// </summary>
+    [SerializeField]
+    private GameObject _fallEssence;
+
+    /// <summary>
+    /// The winter essence to find
+    /// </summary>
+    [SerializeField]
+    private GameObject _winterEssence;
+
+    /// <summary>
+    /// The held spring essence
+    /// </summary>
+    [SerializeField]
+    private GameObject _heldSpringEssence;
+
+    /// <summary>
+    /// The held summer essence
+    /// </summary>
+    [SerializeField]
+    private GameObject _heldSummerEssence;
+
+    /// <summary>
+    /// The held fall essence
+    /// </summary>
+    [SerializeField]
+    private GameObject _heldFallEssence;
+
+    /// <summary>
+    /// The held winter essence
+    /// </summary>
+    [SerializeField]
+    private GameObject _heldWinterEssence;
+
+    /// <summary>
+    /// The spring essence in it's holder
+    /// </summary>
+    [SerializeField]
+    private GameObject _finalSpringEssence;
+
+    /// <summary>
+    /// The summer essence in it's holder
+    /// </summary>
+    [SerializeField]
+    private GameObject _finalSummerEssence;
+
+    /// <summary>
+    /// The fall essence in it's holder
+    /// </summary>
+    [SerializeField]
+    private GameObject _finalFallEssence;
+
+    /// <summary>
+    /// The winter essence in it's holder
+    /// </summary>
+    [SerializeField]
+    private GameObject _finalWinterEssence;
+
+    /// <summary>
+    /// The spring essence holder
+    /// </summary>
+    [SerializeField]
+    private GameObject _springHolder;
+
+    /// <summary>
+    /// The summer essence holder
+    /// </summary>
+    [SerializeField]
+    private GameObject _summerHolder;
+
+    /// <summary>
+    /// The fall essence holder
+    /// </summary>
+    [SerializeField]
+    private GameObject _fallHolder;
+
+    /// <summary>
+    /// The winter essence holder
+    /// </summary>
+    [SerializeField]
+    private GameObject _winterHolder;
+
+    /// <summary>
+    /// The empty object from which to raycast
+    /// </summary>
+    private Transform _rayOrigin;
+
+    /// <summary>
+    /// The max reach for the player
+    /// </summary>
+    [SerializeField]
     private float _maxRayDistance;
 
-    private string _wateringCanName;
-    private string _fountainWaterName;
-    private string _deadGardenName;
+    // Spring puzzle objects
+    private const string WateringCanName = "WATERING_CAN_EMPTY=0";
+    private const string FountainWaterName = "FOUNTAIN_WATER=0";
+    private const string FullWateringCanName = "WATERING_CAN_FULL=1";
+    private const string DeadGardenName = "DEAD_GARDEN=1";
 
-	private void Start()
+    // Summer puzzle objects
+    private const string WoodLogName = "WOOD_LOG=2";
+    private const string TableSawName = "TABLE_SAW=2";
+    private const string AxeHandleName = "AXE_HANDLE=3";
+    private const string AxeHeadName = "AXE_HEAD=3";
+    private const string FinalAxeName = "FINAL_AXE=4";
+    private const string BridgeTreeName = "BRIDGE_TREE=4";
+
+    // Winter puzzle objects
+    private const string GateLockName = "GATE_LOCK=5";
+
+    // Essences in the wild
+    private const string SpringEssenceName = "SPRING_ESSENCE=-1";
+    private const string SummerEssenceName = "SUMMER_ESSENCE=-2";
+    private const string FallEssenceName   = "FALL_ESSENCE=-3";
+    private const string WinterEssenceName = "WINTER_ESSENCE=-4";
+
+    // Essences in their holders
+    private const string FinalSpringEssenceName = "FINAL_SPRING_ESSENCE=-1";
+    private const string FinalSummerEssenceName = "FINAL_SUMMER_ESSENCE=-2";
+    private const string FinalFallEssenceName = "FINAL_FALL_ESSENCE=-3";
+    private const string FinalWinterEssenceName = "FINAL_WINTER_ESSENCE=-4";
+
+    // Essence holders
+    private const string SpringHolderName = "SPRING_HOLDER=-1";
+    private const string SummerHolderName = "SUMMER_HOLDER=-2";
+    private const string FallHolderName   = "FALL_HOLDER=-3";
+    private const string WinterHolderName = "WINTER_HOLDER=-4";
+
+    private bool   _isCurrentlyHolding;
+    private string _currentlyHeldName;
+
+    private void Awake()
     {
-        _wateringCanName = _wateringCan.name;
-        _fountainWaterName = _fountainWater.name;
-        _deadGardenName = _deadGarden.name;
+        _currentlyHeldName = string.Empty;
+    }
+
+    private void Start()
+    {
+        _wateringCan.name = WateringCanName;
+        _fountainWater.name = FountainWaterName;
+        _deadGarden.name = DeadGardenName;
 	}
 	
 	private void Update()
@@ -42,10 +244,219 @@ public class PlayerPuzzleMechanics : MonoBehaviour
         if (Input.GetMouseButtonDown(0))
         {
             RaycastHit hit;
-            Physics.Raycast(_rayOrigin, Vector3.forward, out hit, _maxRayDistance);
+            Physics.Raycast(_rayOrigin.position, Vector3.forward, out hit, _maxRayDistance);
 
-            hit.collider.name;
+            string hitName = hit.collider.name;
+
+            switch (hitName)
+            {
+                case WateringCanName:
+                case WoodLogName:
+                case SpringEssenceName:
+                case SummerEssenceName:
+                case FallEssenceName:
+                case WinterEssenceName:
+                case FinalSpringEssenceName:
+                case FinalSummerEssenceName:
+                case FinalFallEssenceName:
+                case FinalWinterEssenceName:
+                {
+                    TryHold(hitName);
+                    break;
+                }
+
+                case FountainWaterName:
+                case DeadGardenName:
+                case TableSawName:
+                case AxeHeadName:
+                case BridgeTreeName:
+                case GateLockName:
+                case SpringHolderName:
+                case SummerHolderName:
+                case FallHolderName:
+                case WinterHolderName:
+                {
+                    TryActivate(hit.collider.name);
+                    break;
+                }
+            }
         }
 	}
 
+    private void TryHold(string objectNameToHold)
+    {
+        if (!_isCurrentlyHolding)
+        {
+            _currentlyHeldName = objectNameToHold;
+
+            switch (_currentlyHeldName)
+            {
+                case WateringCanName:
+                {
+                    _wateringCan.SetActive(false);
+                    _heldWateringCan.SetActive(true);
+                    break;
+                }
+                case WoodLogName:
+                {
+                    _woodLog.SetActive(false);
+                    _heldWoodLog.SetActive(true);
+                    break;
+                }
+                case SpringEssenceName:
+                {
+                    _springEssence.SetActive(false);
+                    _heldSpringEssence.SetActive(true);
+                    break;
+                }
+                case SummerEssenceName:
+                {
+                    _summerEssence.SetActive(false);
+                    _heldSummerEssence.SetActive(true);
+                    break;
+                }
+                case FallEssenceName:
+                {
+                    _fallEssence.SetActive(false);
+                    _heldFallEssence.SetActive(true);
+                    break;
+                }
+                case WinterEssenceName:
+                {
+                    _winterEssence.SetActive(false);
+                    _heldWinterEssence.SetActive(true);
+                    break;
+                }
+                case FinalSpringEssenceName:
+                {
+                    _finalSpringEssence.SetActive(false);
+                    _heldSpringEssence.SetActive(true);
+                    break;
+                }
+                case FinalSummerEssenceName:
+                {
+                    _finalSummerEssence.SetActive(false);
+                    _heldSummerEssence.SetActive(true);
+                    break;
+                }
+                case FinalFallEssenceName:
+                {
+                    _finalFallEssence.SetActive(false);
+                    _heldFallEssence.SetActive(true);
+                    break;
+                }
+                case FinalWinterEssenceName:
+                {
+                    _finalWinterEssence.SetActive(false);
+                    _heldWinterEssence.SetActive(true);
+                    break;
+                }
+            }
+        }
+        else
+        {
+            // TODO: display "Can't hold" message
+        }
+    }
+
+    private void TryActivate(string objectNameToActivate)
+    {
+        if (_isCurrentlyHolding)
+        {
+            int heldId = int.Parse(_currentlyHeldName.Split('=')[1]);
+            int objectId = int.Parse(objectNameToActivate.Split('=')[1]);
+
+            if (heldId == objectId)
+            {
+                switch (heldId)
+                {
+                    // Fill the watering can
+                    case 0:
+                    {
+                        _heldWateringCan.SetActive(false);
+                        _heldWateringCanFull.SetActive(true);
+                        _currentlyHeldName = FullWateringCanName;
+                        break;
+                    }
+
+                    // Water the garden
+                    case 1:
+                    {
+                        _heldWateringCanFull.SetActive(false);
+                        _heldWateringCan.SetActive(true);
+                        _currentlyHeldName = string.Empty;
+                        break;
+                    }
+
+                    // Cut the log
+                    case 2:
+                    {
+                        _heldWoodLog.SetActive(false);
+                        _heldAxeHandle.SetActive(true);
+                        _currentlyHeldName = AxeHandleName;
+                        break;
+                    }
+
+                    // Create the axe
+                    case 3:
+                    {
+                        _heldAxeHandle.SetActive(false);
+                        _heldAxeFinal.SetActive(true);
+                        _currentlyHeldName = FinalAxeName;
+                        break;
+                    }
+
+                    // Chop the tree
+                    case 4:
+                    {
+                        _heldAxeFinal.SetActive(false);
+                        _currentlyHeldName = string.Empty;
+                        break;
+                    }
+
+                    // Put spring essence in it's holder
+                    case -1:
+                    {
+                        _heldSpringEssence.SetActive(false);
+                        _finalSpringEssence.SetActive(true);
+                        _currentlyHeldName = string.Empty;
+                        break;
+                    }
+
+                    // Put summer essence in it's holder
+                    case -2:
+                    {
+                        _heldSummerEssence.SetActive(false);
+                        _finalSummerEssence.SetActive(true);
+                        _currentlyHeldName = string.Empty;
+                        break;
+                    }
+
+                    // Put fall essence in it's holder
+                    case -3:
+                    {
+                        _heldFallEssence.SetActive(false);
+                        _finalFallEssence.SetActive(true);
+                        _currentlyHeldName = string.Empty;
+                        break;
+                    }
+
+                    // Put winter essence in it's holder
+                    case -4:
+                    {
+                        _heldWinterEssence.SetActive(false);
+                        _finalWinterEssence.SetActive(true);
+                        _currentlyHeldName = string.Empty;
+                        break;
+                    }
+                }
+            }
+            // Disgusting special case to melt the ice
+            else if (heldId == int.Parse(SummerEssenceName.Split('=')[1]) &&
+                     objectId == int.Parse(GateLockName.Split('=')[1]))
+            {
+                _gateLock.SetActive(false);
+            }
+        }
+    }
 }
